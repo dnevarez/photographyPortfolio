1d5ef6a615b885ce0c4171cc2eb61b69a5bd9d8b
Dan Nevarez <dnev.dev@gmail.com>
2016-10-13 14:24:53 -0600

    fix nav for carousel

--- dist/all.js
+++ dist/all.js
@@ -40,9 +40,20 @@ angular.module('photo').controller('landingCtrl', function ($scope) {
 
   $(document).ready(function () {
     $('.your-class').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
       arrows: false,
-      dots: true,
-      draggable: true
+      fade: true,
+      asNavFor: '.multiple-items'
+    });
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.your-class',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true
     });
   });
 });
